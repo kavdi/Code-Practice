cb90cd6d6f46ccdef39c2be41179e1e7d995b39a
kavdi <kavdyjh@gmail.com>
2021-01-02 13:06:22 -0800

    working on Bin heap data structure.

---
+++ py-data_structures/bin_heap.py
@@ -0,0 +1,29 @@
+"""Create a Binary Heap class."""
+
+
+class Node:
+    """ Create Node class with attributes."""
+
+    def __init__(self, data):
+        self.data = data
+        self.parent = None
+        self.c1 = None
+        self.c2 = None
+        self.index = 0
+
+
+class BinHeap:
+    """Implement a Binary Heap."""
+
+    def __init__(self, iterable=None):
+        """Inistialize the Bin Heap."""
+        self.root = None
+        self.values = [None]
+        if isinstance(iterable, (str, list, tuple)):
+            for item in iterable:
+                self.insert(Node)
+
+"""
+insert node
+remove node
+search
